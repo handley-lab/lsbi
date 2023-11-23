802111de3a0e57ad8b9bb8e3de0c7586a36ede70
Will Handley <wh260@cam.ac.uk>
2023-11-23 16:57:00 +0000

    First draft pre-local testing

--- lsbi/stats.py
+++ lsbi/stats.py
@@ -5,7 +5,7 @@ from numpy.linalg import inv
 from scipy.special import erf, logsumexp
 from scipy.stats._multivariate import multivariate_normal_frozen
 
-from lsbi.utils import bisect
+from lsbi.utils import bisect, logdet
 
 
 class multivariate_normal(multivariate_normal_frozen):  # noqa: D101
@@ -77,6 +77,116 @@ class multivariate_normal(multivariate_normal_frozen):  # noqa: D101
             return self.mean + np.einsum("ij,...j->...i", L, y)
 
 
+class multimultivariate_normal(object):
+    """Multivariate normal distribution with multiple means and covariances.
+
+    Implemented with the same style as scipy.stats.multivariate_normal
+
+    Parameters
+    ----------
+    means : array_like, shape (n_components, n_features)
+        Mean of each component.
+
+    covs: array_like, shape (n_components, n_features, n_features)
+        Covariance matrix of each component.
+
+    """
+
+    def __init__(self, means, covs):
+        self.means = np.array([np.atleast_1d(m) for m in means])
+        self.covs = np.array([np.atleast_2d(c) for c in covs])
+
+    def logpdf(self, x):
+        """Log of the probability density function."""
+        process_quantiles = scipy.stats.multivariate_normal._process_quantiles
+        x = process_quantiles(x, self.means.shape)
+        dx = self.means - x[..., :, :]
+        invcovs = np.linalg.inv(self.covs)
+        chi2 = np.einsum("...ij,ijk,...ik->...i", dx, invcovs, dx)
+        norm = -logdet(2 * np.pi * self.covs) / 2
+        logpdf = norm - chi2 / 2
+        return logpdf
+
+    def rvs(self, size=1):
+        """Random variates."""
+        size = np.atleast_1d(size)
+        x = np.random.randn(*size, self.means.shape[-2], self.means.shape[-1])
+        choleskys = np.linalg.cholesky(self.covs)
+        return np.squeeze(self.means + np.einsum("ijk,...ik->...ij", choleskys, x))
+
+    def marginalise(self, indices):
+        """Marginalise over indices.
+
+        Parameters
+        ----------
+        indices : array_like
+            Indices to marginalise.
+        """
+        i = self._bar(indices)
+        means = self.means[:, i]
+        covs = self.covs[:, i][:, :, i]
+        return multimultivariate_normal(means, covs)
+
+    def condition(self, indices, values):
+        """Condition on indices with values.
+
+        Parameters
+        ----------
+        indices : array_like
+            Indices to condition over.
+        values : array_like
+            Values to condition on.
+        """
+        i = self._bar(indices)
+        k = indices
+        means = self.means[:, i] + np.einsum(
+            "ija,iab,ib->ij",
+            self.covs[:, i][:, :, k],
+            inv(self.covs[:, k][:, :, k]),
+            (values - self.means[:, k]),
+        )
+        covs = self.covs[:, i][:, :, i] - np.einsum(
+            "ija,iab,ibk->ijk",
+            self.covs[:, i][:, :, k],
+            inv(self.covs[:, k][:, :, k]),
+            self.covs[:, k][:, :, i],
+        )
+        return multimultivariate_normal(means, covs)
+
+    def _bar(self, indices):
+        """Return the indices not in the given indices."""
+        k = np.ones(self.means.shape[-1], dtype=bool)
+        k[indices] = False
+        return k
+
+    def bijector(self, x, inverse=False):
+        """Bijector between U([0, 1])^d and the distribution.
+
+        - x in [0, 1]^d is the hypercube space.
+        - theta in R^d is the physical space.
+
+        Computes the transformation from x to theta or theta to x depending on
+        the value of inverse.
+
+        Parameters
+        ----------
+        x : array_like, shape (..., d)
+            if inverse: x is theta
+            else: x is x
+        inverse : bool, optional, default=False
+            If True: compute the inverse transformation from physical to
+            hypercube space.
+        """
+        Ls = np.linalg.cholesky(self.covs)
+        if inverse:
+            Linvs = inv(Ls)
+            y = np.einsum("ijk,...ik->...ij", Linvs, x - self.means)
+            return scipy.stats.norm.cdf(y)
+        else:
+            y = scipy.stats.norm.ppf(x)
+            return self.means + np.einsum("ijk,...ik->...ij", Ls, y)
+
+
 class mixture_multivariate_normal(object):
     """Mixture of multivariate normal distributions.
 
@@ -106,7 +216,7 @@ class mixture_multivariate_normal(object):
         dx = self.means - x[..., None, :]
         invcovs = np.linalg.inv(self.covs)
         chi2 = np.einsum("...ij,ijk,...ik->...i", dx, invcovs, dx)
-        norm = -np.linalg.slogdet(2 * np.pi * self.covs)[1] / 2
+        norm = -logdet(2 * np.pi * self.covs) / 2
         logpdf = norm - chi2 / 2
         if reduce:
             logA = self.logA - scipy.special.logsumexp(self.logA)
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -4,114 +4,13 @@ import scipy.special
 from numpy.testing import assert_allclose
 from scipy.stats import invwishart, kstest
 
-from lsbi.stats import mixture_multivariate_normal, multivariate_normal
+from lsbi.stats import (
+    mixture_multivariate_normal,
+    multimultivariate_normal,
+    multivariate_normal,
+)
 
 N = 1000
-
-
-@pytest.mark.parametrize("k", [1, 2, 5, 10])
-@pytest.mark.parametrize("d", [1, 2, 5, 10])
-class TestMixtureMultivariateNormal(object):
-    cls = mixture_multivariate_normal
-
-    def random(self, k, d):
-        means = np.random.randn(k, d)
-        covs = invwishart(scale=np.eye(d), df=d * 10).rvs(k)
-        if k == 1:
-            covs = np.array([covs])
-        logA = np.log(scipy.stats.dirichlet(np.ones(k)).rvs())[0] + 10
-        return self.cls(means, covs, logA)
-
-    def test_rvs(self, k, d):
-        dist = self.random(k, d)
-        logA = dist.logA
-        logA -= scipy.special.logsumexp(logA)
-        mvns = [
-            scipy.stats.multivariate_normal(dist.means[i], dist.covs[i])
-            for i in range(k)
-        ]
-
-        samples_1, logpdfs_1 = [], []
-        for _ in range(N):
-            i = np.random.choice(k, p=np.exp(logA))
-            x = mvns[i].rvs()
-            samples_1.append(x)
-            logpdf = scipy.special.logsumexp(
-                [mvns[j].logpdf(x) + logA[j] for j in range(k)]
-            )
-            assert_allclose(logpdf, dist.logpdf(x))
-            logpdfs_1.append(logpdf)
-        samples_1, logpdfs_1 = np.array(samples_1), np.array(logpdfs_1)
-
-        samples_2 = dist.rvs(N)
-        logpdfs_2 = dist.logpdf(samples_2)
-
-        for i in range(d):
-            if d == 1:
-                p = kstest(samples_1, samples_2).pvalue
-            else:
-                p = kstest(samples_1[:, i], samples_2[:, i]).pvalue
-            assert p > 1e-5
-
-        p = kstest(logpdfs_1, logpdfs_2).pvalue
-        assert p > 1e-5
-
-        for shape in [(d,), (3, d), (3, 4, d)]:
-            x = np.random.rand(*shape)
-            assert mvns[0].logpdf(x).shape == dist.logpdf(x).shape
-
-    def test_bijector(self, k, d):
-        k = 1
-        d = 4
-        dist = self.random(k, d)
-
-        # Test inversion
-        x = np.random.rand(N, d)
-        theta = dist.bijector(x)
-        assert_allclose(dist.bijector(theta, inverse=True), x, atol=1e-6)
-
-        # Test sampling
-        samples = dist.rvs(N)
-        for i in range(d):
-            p = kstest(theta[:, i], samples[:, i]).pvalue
-            assert p > 1e-5
-
-        p = kstest(dist.logpdf(samples), dist.logpdf(theta)).pvalue
-        assert p > 1e-5
-
-        # Test shapes
-        x = np.random.rand(d)
-        theta = dist.bijector(x)
-        assert theta.shape == x.shape
-        assert dist.bijector(theta, inverse=True).shape == x.shape
-
-        x = np.random.rand(3, 4, d)
-        theta = dist.bijector(x)
-        assert theta.shape == x.shape
-        assert dist.bijector(theta, inverse=True).shape == x.shape
-
-    @pytest.mark.parametrize("p", np.arange(1, 5))
-    def test_marginalise_condition(self, d, k, p):
-        if d <= p:
-            pytest.skip("d <= p")
-        i = np.random.choice(d, p, replace=False)
-        j = np.array([x for x in range(d) if x not in i])
-        dist = self.random(k, d)
-        mixture_2 = dist.marginalise(i)
-        assert mixture_2.means.shape == (k, d - p)
-        assert mixture_2.covs.shape == (k, d - p, d - p)
-        assert_allclose(dist.means[:, j], mixture_2.means)
-        assert_allclose(dist.covs[:, j][:, :, j], mixture_2.covs)
-
-        v = np.random.randn(k, p)
-        mixture_3 = dist.condition(i, v)
-        assert mixture_3.means.shape == (k, d - p)
-        assert mixture_3.covs.shape == (k, d - p, d - p)
-
-        v = np.random.randn(p)
-        mixture_3 = dist.condition(i, v)
-        assert mixture_3.means.shape == (k, d - p)
-        assert mixture_3.covs.shape == (k, d - p, d - p)
 
 
 @pytest.mark.parametrize("d", [1, 2, 5, 10])
@@ -194,3 +93,202 @@ class TestMultivariateNormal(object):
         dist_3 = dist_1.condition(i, v)
         assert dist_3.mean.shape == (d - p,)
         assert dist_3.cov.shape == (d - p, d - p)
+
+
+@pytest.mark.parametrize("k", [1, 2, 5, 10])
+@pytest.mark.parametrize("d", [1, 2, 5, 10])
+class TestMultiMultivariateNormal(object):
+    cls = multimultivariate_normal
+
+    def random(self, k, d):
+        means = np.random.randn(k, d)
+        covs = invwishart(scale=np.eye(d), df=d * 10).rvs(k)
+        return self.cls(means, covs)
+
+    def test_rvs(self, k, d):
+        dist = self.random(k, d)
+        mvns = [
+            scipy.stats.multivariate_normal(dist.means[i], dist.covs[i])
+            for i in range(k)
+        ]
+
+        samples_1, logpdfs_1 = [], []
+        for _ in range(N):
+            xs = [mvn.rvs() for mvn in mvns]
+            samples_1.append(xs)
+            logpdf = [mvn.logpdf(x) for x, mvn in zip(xs, mvns)]
+            assert_allclose(logpdf, dist.logpdf(xs))
+            logpdfs_1.append(logpdf)
+        samples_1, logpdfs_1 = np.array(samples_1), np.array(logpdfs_1)
+
+        samples_2 = dist.rvs(N)
+        logpdfs_2 = dist.logpdf(samples_2)
+
+        for j in range(k):
+            for i in range(d):
+                p = kstest(samples_1[:, j, i], samples_2[:, j, i]).pvalue
+                assert p > 1e-5
+
+            p = kstest(logpdfs_1[j], logpdfs_2[j]).pvalue
+            assert p > 1e-5
+
+        for shape in [(k, d), (3, k, d), (3, 4, k, d)]:
+            xs = np.random.rand(*shape)
+            logpdfs = [mvn.logpdf(x) for x, mvn in zip(x, mvns)]
+            assert np.shape(logpdfs) == dist.logpdf(x).shape
+
+    def test_bijector(self, k, d):
+        dist = self.random(k, d)
+
+        # Test inversion
+        xs = np.random.rand(N, k, d)
+        theta = dist.bijector(xs)
+        assert_allclose(dist.bijector(theta, inverse=True), xs, atol=1e-6)
+
+        # Test sampling
+        samples = dist.rvs(N)
+        logpdf_1 = dist.logpdf(samples)
+        logpdf_2 = dist.logpdf(theta)
+        for j in range(k):
+            for i in range(d):
+                p = kstest(theta[:, j, i], samples[:, j, i]).pvalue
+                assert p > 1e-5
+            p = kstest(logpdf_1[j], logpdf_2[j]).pvalue
+            assert p > 1e-5
+
+        # Test shapes
+        xs = np.random.rand(k, d)
+        theta = dist.bijector(xs)
+        assert theta.shape == xs.shape
+        assert dist.bijector(theta, inverse=True).shape == xs.shape
+
+        xs = np.random.rand(3, 4, k, d)
+        theta = dist.bijector(xs)
+        assert theta.shape == xs.shape
+        assert dist.bijector(theta, inverse=True).shape == xs.shape
+
+    @pytest.mark.parametrize("p", np.arange(1, 5))
+    def test_marginalise_condition(self, d, k, p):
+        if d <= p:
+            pytest.skip("d <= p")
+        i = np.random.choice(d, p, replace=False)
+        j = np.array([x for x in range(d) if x not in i])
+        dist = self.random(k, d)
+        mixture_2 = dist.marginalise(i)
+        assert mixture_2.means.shape == (k, d - p)
+        assert mixture_2.covs.shape == (k, d - p, d - p)
+        assert_allclose(dist.means[:, j], mixture_2.means)
+        assert_allclose(dist.covs[:, j][:, :, j], mixture_2.covs)
+
+        v = np.random.randn(k, p)
+        mixture_3 = dist.condition(i, v)
+        assert mixture_3.means.shape == (k, d - p)
+        assert mixture_3.covs.shape == (k, d - p, d - p)
+
+        v = np.random.randn(p)
+        mixture_3 = dist.condition(i, v)
+        assert mixture_3.means.shape == (k, d - p)
+        assert mixture_3.covs.shape == (k, d - p, d - p)
+
+
+@pytest.mark.parametrize("k", [1, 2, 5, 10])
+@pytest.mark.parametrize("d", [1, 2, 5, 10])
+class TestMixtureMultivariateNormal(object):
+    cls = mixture_multivariate_normal
+
+    def random(self, k, d):
+        means = np.random.randn(k, d)
+        covs = invwishart(scale=np.eye(d), df=d * 10).rvs(k)
+        if k == 1:
+            covs = np.array([covs])
+        logA = np.log(scipy.stats.dirichlet(np.ones(k)).rvs())[0] + 10
+        return self.cls(means, covs, logA)
+
+    def test_rvs(self, k, d):
+        dist = self.random(k, d)
+        logA = dist.logA
+        logA -= scipy.special.logsumexp(logA)
+        mvns = [
+            scipy.stats.multivariate_normal(dist.means[i], dist.covs[i])
+            for i in range(k)
+        ]
+
+        samples_1, logpdfs_1 = [], []
+        for _ in range(N):
+            i = np.random.choice(k, p=np.exp(logA))
+            x = mvns[i].rvs()
+            samples_1.append(x)
+            logpdf = scipy.special.logsumexp(
+                [mvns[j].logpdf(x) + logA[j] for j in range(k)]
+            )
+            assert_allclose(logpdf, dist.logpdf(x))
+            logpdfs_1.append(logpdf)
+        samples_1, logpdfs_1 = np.array(samples_1), np.array(logpdfs_1)
+
+        samples_2 = dist.rvs(N)
+        logpdfs_2 = dist.logpdf(samples_2)
+
+        for i in range(d):
+            if d == 1:
+                p = kstest(samples_1, samples_2).pvalue
+            else:
+                p = kstest(samples_1[:, i], samples_2[:, i]).pvalue
+            assert p > 1e-5
+
+        p = kstest(logpdfs_1, logpdfs_2).pvalue
+        assert p > 1e-5
+
+        for shape in [(d,), (3, d), (3, 4, d)]:
+            x = np.random.rand(*shape)
+            assert mvns[0].logpdf(x).shape == dist.logpdf(x).shape
+
+    def test_bijector(self, k, d):
+        dist = self.random(k, d)
+
+        # Test inversion
+        x = np.random.rand(N, d)
+        theta = dist.bijector(x)
+        assert_allclose(dist.bijector(theta, inverse=True), x, atol=1e-6)
+
+        # Test sampling
+        samples = dist.rvs(N)
+        for i in range(d):
+            p = kstest(theta[:, i], samples[:, i]).pvalue
+            assert p > 1e-5
+
+        p = kstest(dist.logpdf(samples), dist.logpdf(theta)).pvalue
+        assert p > 1e-5
+
+        # Test shapes
+        x = np.random.rand(d)
+        theta = dist.bijector(x)
+        assert theta.shape == x.shape
+        assert dist.bijector(theta, inverse=True).shape == x.shape
+
+        x = np.random.rand(3, 4, d)
+        theta = dist.bijector(x)
+        assert theta.shape == x.shape
+        assert dist.bijector(theta, inverse=True).shape == x.shape
+
+    @pytest.mark.parametrize("p", np.arange(1, 5))
+    def test_marginalise_condition(self, d, k, p):
+        if d <= p:
+            pytest.skip("d <= p")
+        i = np.random.choice(d, p, replace=False)
+        j = np.array([x for x in range(d) if x not in i])
+        dist = self.random(k, d)
+        mixture_2 = dist.marginalise(i)
+        assert mixture_2.means.shape == (k, d - p)
+        assert mixture_2.covs.shape == (k, d - p, d - p)
+        assert_allclose(dist.means[:, j], mixture_2.means)
+        assert_allclose(dist.covs[:, j][:, :, j], mixture_2.covs)
+
+        v = np.random.randn(k, p)
+        mixture_3 = dist.condition(i, v)
+        assert mixture_3.means.shape == (k, d - p)
+        assert mixture_3.covs.shape == (k, d - p, d - p)
+
+        v = np.random.randn(p)
+        mixture_3 = dist.condition(i, v)
+        assert mixture_3.means.shape == (k, d - p)
+        assert mixture_3.covs.shape == (k, d - p, d - p)
