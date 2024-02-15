d6c3996769e76499ef24769a326aa291b49461d7
Will Handley <wh260@cam.ac.uk>
2024-02-15 16:22:40 +0000

    Added a test for alias and model kwargs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
 import pytest
 from numpy.testing import assert_allclose
 
-from lsbi.utils import bisect
+from lsbi.utils import bisect, alias
+import numpy as np
 
 
 def test_bisect():
@@ -17,3 +18,24 @@ def test_bisect():
         return x - [1, 2]
 
     assert_allclose(bisect(f, 0, 10), [1, 2])
+
+
+def test_alias():
+    class A:
+        pass
+
+    a = A()
+    with pytest.raises(AttributeError):
+        a.x
+    a.x = 1
+    assert a.x == 1
+    with pytest.raises(AttributeError):
+        a.y
+    alias(A, "x", "y")
+    assert a.y == 1
+    a.y = 2
+    assert a.x == 2
+    a.x = np.eye(3)
+    assert_allclose(a.y, np.eye(3))
+    a.y[0, 0] = 0
+    assert a.x[0, 0] == 0
