Actually fixed broadcasting now

lsbi/stats.py
@@ -463,20 +463,22 @@ class mixture_normal(multivariate_normal):
         """
         if self.shape == ():
             return super().rvs(size=size, broadcast=broadcast)
-        if broadcast:
-            shape = np.broadcast_shapes(size, self.shape[:-1])
-        else:
-            shape = (size, *self.shape[:-1])
         size = np.atleast_1d(np.array(size, dtype=int))
         logw = np.broadcast_to(self.logw, self.shape).copy()
         logw = logw - logsumexp(logw, axis=-1, keepdims=True)
         p = np.exp(logw)
         cump = np.cumsum(p, axis=-1)
-        u = np.random.rand(np.prod(shape)).reshape(-1, *p.shape[:-1])
+        if broadcast:
+            u = np.random.rand(*size).reshape(-1, *p.shape[:-1])
+        else:
+            u = np.random.rand(np.prod(size), *p.shape[:-1])
         i = np.argmax(np.array(u)[..., None] < cump, axis=-1)
         mean = np.broadcast_to(self.mean, (*self.shape, self.dim))
         mean = np.take_along_axis(np.moveaxis(mean, -2, 0), i[..., None], axis=0)
-        x = np.random.randn(np.prod(shape)).reshape(-1, *self.shape[:-1], self.dim)
+        if broadcast:
+            x = np.random.randn(*size, self.dim)
+        else:
+            x = np.random.randn(np.prod(size), *self.shape[:-1], self.dim)
         if self.diagonal:
             L = np.sqrt(self.cov)
             L = np.broadcast_to(L, (*self.shape, self.dim))